aac3c591ef70a68380368c97ce7aec1b1d016a0d
longmajuxie <maxingxiang_1@163.com>
2017-11-21 22:53:32 +0800

    antd+layout

---
+++ src/main.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { SubMenu } = Menu;
+const { Header, Content, Sider,Footer } = Layout;
+const MenuItemGroup = Menu.ItemGroup;
+
+class App extends Component {
+    state = {
+        current: 'mail',
+    }
+    handleClick = (e) => {
+        console.log('click ', e);
+        this.setState({
+            current: e.key,
+        });
+    }
+  render() {
+    return (
+        <Layout>
+            <Header className="header">
+                <div className="logo" />
+                <Menu
+                    onClick={this.handleClick}
+                    selectedKeys={[this.state.current]}
+                    mode="horizontal"
+                    style={{ lineHeight: '64px' }}
+                    theme="dark"
+                >
+                    <Menu.Item key="mail">
+                        <Icon type="mail" />Navigation One
+                    </Menu.Item>
+                    <Menu.Item key="app" disabled>
+                        <Icon type="appstore" />Navigation Two
+                    </Menu.Item>
+                    <SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
+                        <MenuItemGroup title="Item 1">
+                            <Menu.Item key="setting:1">Option 1</Menu.Item>
+                            <Menu.Item key="setting:2">Option 2</Menu.Item>
+                        </MenuItemGroup>
+                        <MenuItemGroup title="Item 2">
+                            <Menu.Item key="setting:3">Option 3</Menu.Item>
+                            <Menu.Item key="setting:4">Option 4</Menu.Item>
+                        </MenuItemGroup>
+                    </SubMenu>
+                    <Menu.Item key="alipay">
+                        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
+                    </Menu.Item>
+                </Menu>
+            </Header>
+            <Layout>
+                <Sider width={200} style={{ background: '#fff' }}>
+                    <Menu
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        defaultOpenKeys={['sub1']}
+                        style={{ height: '100%', borderRight: 0 }}
+                    >
+                        <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                            <Menu.Item key="1">option1</Menu.Item>
+                            <Menu.Item key="2">option2</Menu.Item>
+                            <Menu.Item key="3">option3</Menu.Item>
+                            <Menu.Item key="4">option4</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+                            <Menu.Item key="5">option5</Menu.Item>
+                            <Menu.Item key="6">option6</Menu.Item>
+                            <Menu.Item key="7">option7</Menu.Item>
+                            <Menu.Item key="8">option8</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+                            <Menu.Item key="9">option9</Menu.Item>
+                            <Menu.Item key="10">option10</Menu.Item>
+                            <Menu.Item key="11">option11</Menu.Item>
+                            <Menu.Item key="12">option12</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout style={{ padding: '0 24px 24px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+        </Layout>
+    );
+  }
+}
+
+export default App;
